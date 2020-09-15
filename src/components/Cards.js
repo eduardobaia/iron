6fd302b7abd9b75bb2188dccc7a7eeb9300fd43a
eduardobaia <eduardo_baia@msn.com>
2020-09-15 11:54:42 +0100

    firtt commit menu

---
+++ src/components/Cards.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import CardItem from './CardItem';
+import './Cards.css';
+
+function Cards() {
+    return (
+        <div className="cards">
+            <h1>Training outdoor!</h1>
+            <div className="cards__container">
+                <div className="cards__wrapper">
+                    <ul className="cards__items">
+                        <CardItem
+                            src='images/out.jpg'
+                            text="Explore your limits inside nature"
+                            label='Adventure'
+                            path='/Services'
+                        />
+
+                        <CardItem
+                            src='images/95.jpg'
+                            text="Test test "
+                            label='Adventure'
+                            path='/Services'
+                        />
+
+
+                        <CardItem
+                            src='images/cross.jpeg'
+                            text="Explore your limits inside nature"
+                            label='Adventure'
+                            path='/Services'
+                        />
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cards
